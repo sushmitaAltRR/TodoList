e4eeb547bbe0f0d73622ec27b738a067aad328f3
sushmitaAltRR <sushmita@altrr.in>
2025-01-17 16:39:24 +0530

    second commit

--- src/components/ListTodo.js
+++ src/components/ListTodo.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function ListTodo(props) {
   return (
     <ul>
-      <li className="flex items-center justify-between p-1 bg-white rounded-lg shadow-md">
-        {props.item}
+      <li className="flex items-center justify-between p-2 bg-white rounded-lg shadow-md">
+        {/* Task text */}
+        <span className="flex-1 text-left truncate">{props.item}</span>
 
         {/* Delete button with hover effect */}
         <button
-          className=" ml-80 hover:bg-red-800 p-2 rounded-full"
+          className="ml-2 hover:bg-red-800 p-2 rounded-full"
           onClick={() => props.deleteItems(props.index)} // Delete task
         >
           <FontAwesomeIcon icon={faTrash} className="text-red-600 hover:text-white" />
@@ -18,7 +19,7 @@ function ListTodo(props) {
 
         {/* Edit button with hover effect */}
         <button
-          className="ml-1 hover:bg-yellow-500 p-2 rounded-full"
+          className="ml-2 hover:bg-yellow-500 p-2 rounded-full"
           onClick={() => props.startEdit(props.index)} // Start editing task
         >
           <FontAwesomeIcon icon={faEdit} className="text-gray-700 hover:text-white" />
